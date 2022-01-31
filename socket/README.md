b8d2b13c81ae233853c5b4cbab8b1a970573c894
onlyhappiness <onlyhappiness96@gmail.com>
2022-01-31 23:43:11 +0900

    Create socket.io server

---
+++ socket/README.md
@@ -0,0 +1,10 @@
+## 참고
+
+### socket server
+https://socket.io/docs/v4/server-initialization/
+
+
+## 홓! 포트 3000 중복되요!
+> 이거 참고 ㄱㄱ
+
+https://socket.io/docs/v4/client-initialization/
--- socket/index.js
+++ socket/index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+import { Server } from "socket.io";
+
+console.log(
+  "서버를 키면 이것부터 콘솔로 출력하셈"
+);
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("하응.. 연결됨..");
+
+  socket.on("disconnect", () => {
+    console.log("하응.. 대화상대 나감..");
+  });
+});
+
+io.listen(3000);
